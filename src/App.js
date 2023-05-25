f54d534926ae614a616fd8830d83f162f70616ee
ManhDai <56792079+manh4702@users.noreply.github.com>
2023-05-25 21:44:05 +0700

    my project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import 'react-datepicker/dist/react-datepicker.css';
 
 function App() {
+  const [day, setDay] = useState('');
+  const [month, setMonth] = useState('');
+  const [year, setYear] = useState('');
+
+  const handleCheckDate = () => {
+    if (!day || !month || !year) {
+      alert('Vui lòng nhập đầy đủ thông tin ngày, tháng, năm!');
+      return;
+    }
+
+    const parsedDay = parseInt(day);
+    const parsedMonth = parseInt(month);
+    const parsedYear = parseInt(year);
+
+    if (isNaN(parsedDay) || isNaN(parsedMonth) || isNaN(parsedYear)) {
+      alert('Vui lòng nhập đúng định dạng ngày, tháng, năm!');
+      return;
+    }
+
+    if ((parsedYear % 4 === 0 && parsedYear % 100 === 0) || parsedYear % 400 === 0) {
+      alert('Năm nhuận!');
+    }
+
+    if (parsedMonth < 1 || parsedMonth > 12) {
+      alert('Tháng không hợp lệ!');
+      return;
+    }
+    if (parsedDay < 1 || parsedDay > 31) {
+      alert('Ngày không hợp lệ!');
+      return;
+    }
+    if (parsedMonth === 2 && parsedDay > 28) {
+      alert('Ngày không hợp lệ!');
+      return;
+    }
+    if ((parsedMonth === 4 || parsedMonth === 6 || parsedMonth === 9 || parsedMonth === 11) && parsedDay > 30) {
+      alert('Ngày không hợp lệ!');
+      return;
+    }
+
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='input-container'>
+      <h1>Date Time Checker</h1>
+      <div>
+        <label>Day: </label>
+        <input type='text' value={day} onChange={(e) => setDay(e.target.value)} />
+      </div>
+      <div>
+        <label>Month</label>
+        <input type='text' value={month} onChange={(e) => setMonth(e.target.value)} />
+        </div>
+      <div>
+        <label>Year</label>
+        <input type='text' value={year} onChange={(e) => setYear(e.target.value)} />
+        </div>
+      <div>
+        <button onClick={handleCheckDate}>Check Date</button>
+        </div>
+      </div>
   );
 }
-
 export default App;
